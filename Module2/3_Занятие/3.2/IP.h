b72101e86edb1da8e2898f45b74804834983fd1f
Alina Tereshkina <lina.tereshkina.02@mail.ru>
2024-04-07 02:42:41 +0700

    3.2 tash new

--- Module2/3_Занятие/3.2/IP.h
+++ Module2/3_Занятие/3.2/IP.h
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <time.h>
 __uint32_t matchingIP(__uint32_t Net_Dst, __uint32_t *Net_Src, int num);
-__uint32_t compareIP(char *IP_Dst, char *Mask, __uint32_t *IP_Src, int num, __uint32_t *Net_Src);
+__uint32_t compareIP(char *IP_Dst,__uint32_t mask);
+__uint32_t *compareNet_Src(__uint32_t mask, int num, __uint32_t *IP_Src, __uint32_t *Net_Src);
 __uint32_t *generation(int num);
 
 #endif
